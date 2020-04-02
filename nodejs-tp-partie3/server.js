4128c54c0c45607f37ea1bb6097d35b62133be81
DupuyAntoine <a.dupuy.pro@gmail.com>
2020-04-02 12:32:36 +0200

    feat: delete on cities table

--- nodejs-tp-partie3/server.js
+++ nodejs-tp-partie3/server.js
@@ -25,6 +25,27 @@ app.use((req, res, next) => {
 })
 app.use(jsonParser)
 app.use(urlEncoded)
+
+router.use( function( req, res, next ) {
+    // this middleware will call for each requested
+    // and we checked for the requested query properties
+    // if _method was existed
+    // then we know, clients need to call DELETE request instead
+    if ( req.query._method == 'DELETE' ) {
+        // change the original METHOD
+        // into DELETE method
+        req.method = 'DELETE';
+        // and set requested url to /user/12
+        req.url = req.path;
+    }
+    if (req.query._method == 'PUT') {
+        req.method = 'PUT'
+        req.url = req.path
+    }      
+    next(); 
+});
+
+
 app.use(router)
 
 // call city routes
